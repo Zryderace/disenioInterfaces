79d44f40a509f7c8e976c74eae7f1416df546a54
zryderace <alemoled@gmail.com>
2025-02-11 17:58:47 +0100

    home y test

--- practica4/js/home.js
+++ practica4/js/home.js
@@ -4,11 +4,37 @@ window.onload=function(){
         $('html, body').animate({
             scrollTop: $(target).offset().top
         }, 1000)
+        $("#oculto").css("display","block")
     })
-    $('#formulario').on('click', function() {
+    $('#formulario button').on('click', function() {
         var target = $("#ofertas");
         $('html, body').animate({
-            scrollTop: $(target).offset().top
+            scrollTop: 0
         }, 1000)
     })
+    $("img").on("mouseenter",function(){
+        $(this).css({"transform":"scale(1.1)",
+                    "border": "5px solid black",
+                    "transition":"transform 0.3 ease"})
+    })
+    $("img").on("mouseleave",function(){
+        $(this).css({"transform":"scale(1)",
+                    "border": "0px",
+                    "transition":"transform 0.3 ease"})
+    })
+
+    var paisCodigo = {  "espana":"+34",
+                        "canada":"+1",
+                        "mexico":"+52",
+                        "eeuu":"+1"
+    }
+
+    $("#pais").on("change", function(){
+        let seleccionado = $(this).val()
+
+        $("#prefijo").val(paisCodigo[seleccionado])
+
+    })
+
+
 }
